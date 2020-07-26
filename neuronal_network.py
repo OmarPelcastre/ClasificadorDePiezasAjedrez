22fae812b5330bada9d58a478e0e66d6cf1ad91d
Omar Pelcastre <omarpelcastre@hotmail.com>
2020-07-26 16:40:14 -0500

    menos pasos de validacion

--- neuronal_network.py
+++ neuronal_network.py
@@ -25,14 +25,14 @@ epocas=8
 longitud, altura = 150, 150
 batch_size = 32
 pasos = 50
-validation_steps = 300
+validation_steps = 200
 filtrosConv1 = 32
 filtrosConv2 = 64
 tamano_filtro1 = (3, 3)
 tamano_filtro2 = (2, 2)
 tamano_pool = (2, 2)
 clases = 3
-lr = 0.0004
+lr = 0.0005
 
 
 
