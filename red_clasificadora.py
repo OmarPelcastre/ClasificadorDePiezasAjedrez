ec5379fc3ecc2f6072345bf346a0672ecd58dbac
Omar Pelcastre <omarpelcastre@hotmail.com>
2020-07-30 17:44:51 -0500

    nueva red

--- red_clasificadora.py
+++ red_clasificadora.py
@@ -7,33 +7,43 @@ from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
 from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
 from tensorflow.python.keras import backend as K
 from tensorflow.python.framework.ops import disable_eager_execution
+from keras.applications import VGG16
+
+
+def create_model():
+    model=applications.vgg16.VGG16()
+    cnn=Sequential()
+    for capa in model.layers:
+        cnn.add(capa)
+    cnn.layers.pop()
+    for layer in cnn.layers:
+        layer.trainable=False
+    cnn.add(Dense(6,activation='softmax'))
+    
+    return cnn
+
 
 disable_eager_execution()
-
-
 K.clear_session()
-
-
 
 data_entrenamiento = './data/Train'
 data_validacion = './data/Validation'
+numeroDePiezas = 6
 
-"""
-Parameters
-"""
-epocas=10
-longitud, altura = 224, 224
-batch_size = 16
-pasos = 20
-validation_steps = 20
+
+
 filtrosConv1 = 32
 filtrosConv2 = 64
 tamano_filtro1 = (3, 3)
 tamano_filtro2 = (2, 2)
 tamano_pool = (2, 2)
-clases = 6
-lr = 0.0005
+learningRate = 0.0005
 
+batch_size = 16
+steps_per_epoch = 50
+validation_steps = 100
+longitud, altura = 224, 224
+epocas=10
 
 
 entrenamiento_datagen = ImageDataGenerator(
@@ -60,25 +70,17 @@ validacion_generador = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-cnn = Sequential()
-cnn.add(Convolution2D(filtrosConv1, tamano_filtro1, padding ="same", input_shape=(longitud, altura, 3), activation='relu'))
-cnn.add(MaxPooling2D(pool_size=tamano_pool))
 
-cnn.add(Convolution2D(filtrosConv2, tamano_filtro2, padding ="same"))
-cnn.add(MaxPooling2D(pool_size=tamano_pool))
-
-cnn.add(Flatten())
-cnn.add(Dense(256, activation='relu'))
-cnn.add(Dropout(0.5))
-cnn.add(Dense(clases, activation='softmax'))
+cnn = create_model()
 
 cnn.compile(loss='categorical_crossentropy',
-            optimizer=optimizers.Adam(lr=lr),
+            optimizer=optimizers.Adam(lr=learningRate),
             metrics=['accuracy'])
+
 
 cnn.fit(
     entrenamiento_generador,
-    steps_per_epoch=pasos,
+    steps_per_epoch=steps_per_epochs,
     epochs=epocas,
     validation_data=validacion_generador,
     validation_steps=validation_steps)
